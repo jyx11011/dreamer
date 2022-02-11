7b5429eae7f44ec513b463d8ef705cc3d8d7dc5f
Jiang Yuxin <jyx11011@gmail.com>
2022-02-11 10:50:24 +0800

    modify

--- cartpole/goal_state.py
+++ cartpole/goal_state.py
@@ -1,11 +1,13 @@
 from dm_control import suite
+import numpy as np
 
 domain="cartpole"
 task="swingup"
 
 env = suite.load(domain_name=domain, task_name=task)
 camera = dict(quadruped=2).get(domain, 0)
-
-f = open(domain+"_"+task+".txt", "a")
-f.write(env.physics.render(64,64,camera_id=camera))
-
+f=domain+"_"+task+".txt"
+obs=env.physics.render(64,64,camera_id=camera)
+print(obs.shape)
+np.savetext(f,obs)
+#export MUJOCO_GL=egl
